730a19bc4259602ba488bbf5c09f9015c6d6d64f
paul <paul@pauldeMBP.lan>
2016-09-01 10:43:48 +0800

    storage

--- sh/nginx/nginx_log_cut.sh
+++ sh/nginx/nginx_log_cut.sh
@@ -3,15 +3,15 @@
 # nginx 访问日志切割
 
 
-logs_path="/m2odata/log/"
-cut_log_path="/m2odata/log/logs/"
+logs_path="/storage/log/"
+cut_log_path="/storage/log/logs/"
 
 date=$(date -d "-1 days" +"%y%m%d")
 ldate=$(date -d "-8 days" +"%y%m%d")  # nginx 访问日志保留7天
 
-pid_path="/m2odata/server/nginx/logs/nginx.pid"
+pid_path="/storage/server/nginx/logs/nginx.pid"
 
-[ -d ${cut_log_path} ] || mkdir -p /m2odata/log/logs
+[ -d ${cut_log_path} ] || mkdir -p /storage/log/logs
 
 mv ${logs_path}access.log ${cut_log_path}access_${date}.log
 
