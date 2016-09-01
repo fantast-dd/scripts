af3aa233f8cde66155977448a972c40350d35826
paul <paul@pauldeMacBook-Pro.local>
2016-09-01 17:16:18 +0800

    disk

---
+++ sh/disk/disk_partition.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# 2016/9/01 pdd
+# 磁盘（小于2TB）自动分区并挂载
+
+DISK="/dev/sdb"
+FSTYPE="ext4"
+
+function storage_fdisk() {
+fdisk $DISK<<EOF
+n
+p
+1
+
+
+w
+EOF
+mkfs -t $FSTYPE ${DISK}1
+}
+
+function storage_mount() {
+cat >>/etc/fstab<<EOF
+${DISK}1                /storage                   ext4    defaults        0 0
+EOF
+mount -a
+}
+
+[ -b ${DISK}1 ] || storage_fdisk
+grep -q ${DISK}1 /etc/fstab || storage_mount
