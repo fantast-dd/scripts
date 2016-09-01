99b609969e2329cf04cef95d3e2255cda027cf35
paul <paul@pauldeMBP.lan>
2016-09-01 10:39:40 +0800

    Architecture change

--- sh/mysql/mysql-perdb_backup.sh
+++ sh/mysql/mysql-perdb_backup.sh
@@ -2,15 +2,15 @@
 # backup every single db of mysql
 
 DATE=`date +%Y%m%d%H%M%S` 
-USER="backupuser"
+USER="backupuser"  # Select and Lock Tables privileges
 PASSWORD="backupuser"
 ##The folder where database save，make sure the"/web/database_back/"folder is exist 
-BACKDIR="/m2odata/backupdb/"`date -d "-1 days" "+%Y%m%d"`
+BACKDIR="/storage/backupdb/"`date -d "-1 days" "+%Y%m%d"`
 
 [ -d ${BACKDIR} ] || mkdir -p ${BACKDIR}
 
 ##Where appropriate changes according to MySQL installation location 
-cd /m2odata/server/mysql/bin/
+cd /storage/server/mysql/bin/
 echo "Backuping Database , please waiting..." 
 for db in `./mysql -u$USER -p$PASSWORD -B -N -e 'show databases like "m2o_%"' | xargs`
 do
@@ -20,5 +20,5 @@ do
 done
 
 ##Here to keep a backup of 7 days
-OBSOLOTESQL="/m2odata/backupdb/"`date -d -8day +%Y%m%d`
+OBSOLOTESQL="/storage/backupdb/"`date -d -8day +%Y%m%d`
 [ -d ${OBSOLOTESQL} ] && rm -rf ${OBSOLOTESQL}
