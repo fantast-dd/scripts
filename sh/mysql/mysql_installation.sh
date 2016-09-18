a5a708e0af7cc06845086046123b816392e30f68
paul <paul@pauldeMacBook-Pro.local>
2016-09-18 22:57:50 +0800

    mysql_installation.sh

--- sh/mysql/mysql_installation.sh
+++ sh/mysql/mysql_installation.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
-# date 2016/8/22 pdd
-# 二进制安装
+# 二进制安装MYSQL
 
-basedir=/storage/server/mysql
-datadir=/storage/data/mysql
+lsof -i:3306 >/dev/null && { echo "3306端口已被占用，请检查是否被其它进程占用，或MYSQL已经安装完成"; exit 1; }
+
+basedir=/m2odata/server/mysql
+datadir=/storage/db
 socket=/tmp/mysql.sock
 user=mysql
-passwd=hogesoft
+password="test"
+[ -n "$password" ] || { echo "Error: 请在配置文件中输入正确的mysql password"; exit 1; }
 
-# 安装包 添加用户名
+# 安装所需包，添加用户名
 grep -q $user /etc/passwd || useradd -M -s /sbin/nologin $user
-yum -y install compat-libstdc++-33.x86_64 libaio.x86_64
+rpm -q --quiet compat-libstdc++-33.x86_64 || yum -y install compat-libstdc++-33.x86_64
+rpm -q --quiet libaio.x86_64 || yum -y install libaio.x86_64
 
-# 安装
-function Install () {
-    cd /usr/local/src
-    tar -xz -f mysql-5.5.43-linux2.6-x86_64.tar.gz
-    [ -d /storage/server ] || mkdir -p /storage/server
-    mv -f mysql-5.5.43-linux2.6-x86_64 /storage/server/
-    ln -sf /storage/server/mysql-5.5.43-linux2.6-x86_64 $basedir
-    chown -R mysql:mysql $basedir
-}
-
-[ -d /storage/server/mysql ] || Install
-
-# 新建mysql数据目录
-[ -d $datadir ] || mkdir -p $datadir;chown -R mysql:mysql $datadir
-
-# 生成新的配置文件
-function Config () {
+# 配置文件
+function config () {
 cat >/etc/my.cnf<<EOF
 [client]
 port=3306
@@ -44,24 +32,59 @@ collation-server=utf8_unicode_ci
 EOF
 }
 
-cd $basedir
-# 初始化
-Config
-./scripts/mysql_install_db --user=$user --basedir=$basedir --datadir=$datadir
+# 安装
+function install () {
+    tar -xz -f mysql-5.6.31-linux-glibc2.5-x86_64.tar.gz
+    \cp -rf mysql-5.6.31-linux-glibc2.5-x86_64 /m2odata/server/mysql
+    chown -R $user:$user $basedir
+}
 
-# 启动
-[ -f /etc/init.d/mysqld ] || cp support-files/mysql.server /etc/init.d/mysqld;chmod 755 /etc/init.d/mysqld
-chkconfig --list mysqld >/dev/null 2>&1 || chkconfig --add mysqld;chkconfig mysqld on
-/etc/init.d/mysqld restart
+# 数据库初始化
+function init_db () {
+    # 新建mysql数据目录
+    [ -d $datadir ] || { mkdir -p $datadir; chown -R $user:$user $datadir; }
+    cd $basedir
+    ./scripts/mysql_install_db --user=$user --basedir=$basedir --datadir=$datadir
+}
+
+# 开机启动
+function self_boot () {
+    [ -f /etc/init.d/mysqld ] || { cp support-files/mysql.server /etc/init.d/mysqld; chmod 755 /etc/init.d/mysqld; }
+    chkconfig --list mysqld >/dev/null  || { chkconfig --add mysqld; chkconfig mysqld on; }
+}
+
+# 添加iptables
+function add_iptables() {
+    grep -w -q 3306 /etc/sysconfig/iptables
+    if [ $? != 0 ];then
+        sed -i '/--dport 22/a -A INPUT -m state --state NEW -m tcp -p tcp --dport 3306 -j ACCEPT' /etc/sysconfig/iptables
+        /etc/init.d/iptables reload
+    fi
+}
 
 # 安全设置
+function security () {
 ./bin/mysql_secure_installation<<EOF
 
 Y
-$passwd
-$passwd
+$password
+$password
 Y  
 Y  
 Y  
 Y
 EOF
+}
+
+if [ ! -d /m2odata/server/mysql ];then
+    config
+    install
+    init_db  # 数据库初始化的时候要读取my.cnf里面的参数
+    self_boot
+    add_iptables
+    /etc/init.d/mysqld start
+    security
+    echo -e "MYSQL安装完成！\n"
+else
+    echo -e "MYSQL已安装！\n"
+fi
