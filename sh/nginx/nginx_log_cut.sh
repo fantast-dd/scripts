ca6805603f7bee5838eda2b15b8f834c1c72c794
fantast-dd <254995740@qq.com>
2016-12-29 16:39:48 +0800

    add error.log

--- sh/nginx/nginx_log_cut.sh
+++ sh/nginx/nginx_log_cut.sh
@@ -3,15 +3,15 @@
 # nginx 访问日志切割
 
 
-logs_path="/storage/log/"
-cut_log_path="/storage/log/logs/"
-pid_path="/storage/server/nginx/logs/nginx.pid"
+logs_path="/storage/log"
+cut_log_path="/storage/log/logs"
+pid_path="/usr/local/nginx/logs/nginx.pid"
 
 date=$(date -d "-1 days" +"%y%m%d")
 ldate=$(date -d "-8 days" +"%y%m%d")  # nginx 访问日志保留7天
 
 [ -d "$cut_log_path" ] || mkdir -p "$cut_log_path"
-mv "${logs_path}access.log" "${cut_log_path}access_${date}.log"
+mv "${logs_path}/access.log" "${cut_log_path}/access_${date}.log"
 kill -USR1 `cat ${pid_path}`
 
 cd "$cut_log_path"
